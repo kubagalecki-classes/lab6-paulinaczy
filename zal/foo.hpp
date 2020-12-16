61c0881901edfafa3be7a12c23a90ad7177844c8
paulinaczy <73669289+paulinaczy@users.noreply.github.com>
2020-12-16 17:31:34 +0100

    Update foo.hpp

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -1,16 +1,27 @@
 #pragma once
 
-#include "Human.hpp"
-
+#include <iostream>
 #include <algorithm>
 #include <list>
 #include <vector>
+#include <iterator>
+#include "Human.hpp"
+
+using std::vector;
+using std::list;
+using std::for_each;
+using std::transform;
+using std::iterator;
+
+void funkcja(Human& ludzik)
+{
+	ludzik.birthday();
+}
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    std::vector< char > ret_v(people.size());
-
-    // Twoja implementacja tutaj
-
-    return ret_v;
+	std::vector< char > ret_v(people.size());
+	std::for_each(people.begin(), people.end(), funkcja);
+	std::transform(people.begin(), people.end(), ret_v.rbegin(), [&](Human& ludzik){if (ludzik.isMonster() == true){return 'n';}else{return 'y';}});
+	return ret_v;
 }
